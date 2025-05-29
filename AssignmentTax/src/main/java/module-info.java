4da042cf85510460ebb950b29ab189d9d2f88996
azimmoinudheen <moinudheen.azim@live.iium.edu.my>
2025-05-29 23:20:02 +0800

    Delete AssignmentTax directory

--- AssignmentTax/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.assignmenttax {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.assignmenttax to javafx.fxml;
-    exports com.example.assignmenttax;
-}
